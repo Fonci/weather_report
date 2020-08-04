58067f8e951e745f0b42bd97cd1444ab4a2009b7
xuanxuan <7807873+xuanxuan1234@user.noreply.gitee.com>
2020-08-04 16:58:06 +0800

    搜索城市信息，实时天气等

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,114 @@
-// 引用百度地图微信小程序JSAPI模块 
-var bmap = require('../../libs/bmap-wx.min');
+
 Page({
   data: {
-    currentWeather: {},
-    dayWeather: [],
-    temperature: '',
-    time: new Date().getHours()
+    location: '',//城市经纬度/位置信息
+    cityName: '',//城市名称
+    searchList: [],//搜索城市列表
+    weatherNow: {},//实时天气
+    weatherHour: {},//24小时天气
+    weatherDaily: {},//7天天气
+    time: new Date().getHours(),//当前小时
+    showSearchList: false,
   },
   onLoad: function () {
+    this.getCurrentCity()
+  },
+  // 获取当前城市
+  getCurrentCity() {
     var that = this;
-    // 新建百度地图对象 
-    var BMap = new bmap.BMapWX({
-      ak: 'iTEqURO4U17I6cGX56MAXeebeu96rRkx'
-    });
-    var fail = function (data) {
-      console.log(data)
-    };
-    var success = function (data) {
-      console.log(data)
-
-      // weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' + '风力：' + weatherData.wind + '\n';
-      that.setData({
-        currentWeather: data.currentWeather[0],
-        dayWeather: data.originalData.results[0].weather_data,
-        temperature: (data.currentWeather[0].date).match(/\：(\S*)\)/)[1]
-      })
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        // 定位当前城市的经纬度
+        that.setData({
+          location: res.longitude + "," + res.latitude
+        })
+        that.getCity(that.data.location);//获取城市
+      }
+    })
+  },
+  // 根据搜索内容 查询出相应城市列表
+  bindInputCity(e) {
+    var that = this;
+    if (e.detail.value) {
+      that.getCity(e.detail.value)
+    } else {
+      that.getCurrentCity()
     }
-    // 发起weather请求 
-    BMap.weather({
-      fail: fail,
-      success: success
-    });
+  },
+  // 获取搜索城市的location id
+  getLocation(options) {
+    this.setData({
+      showSearchList: false
+    })
+    this.getCity(options.target.dataset.location)
+  },
+  // 获取城市信息
+  getCity(location) {
+    var that = this;
+    wx.request({
+      url: 'https://geoapi.heweather.net/v2/city/lookup',
+      data: {
+        location: location,
+        key: "da0ccd314e2849bd8f17e706d07680d1"
+      },
+      success(res) {
+        if (res.data.status == 200) {
+          if (res.data.location.length == 1) {
+            that.setData({
+              cityName: res.data.location[0].adm1 +
+                "-" + res.data.location[0].adm2 +
+                "-" +
+                res.data.location[0].name,
+              location: res.data.location[0].id
+            })
+            that.getWeather('now', that.data.location);//实时天气
+            that.getWeather('24h', that.data.location);//24小时天气
+            that.getWeather('7d', that.data.location);//7天天气
+          } else if (res.data.location.length > 1) {
+            that.setData({
+              searchList: res.data.location
+            })
+            if (that.data.searchList.length == 1 || that.data.searchList.length > 1) {
+              that.setData({
+                showSearchList: true
+              })
+            }
+          }
+
+        }
+
+      }
+    })
+  },
+  getWeather(path, location) {
+    var that = this;
+    wx.request({
+      url: 'https://devapi.heweather.net/v7/weather/' + path,
+      data: {
+        location: location,
+        key: "da0ccd314e2849bd8f17e706d07680d1"
+      },
+      success(res) {
+        if (res.data.code == 200) {
+          if (path == 'now') {
+            that.setData({
+              weatherNow: res.data.now
+            })
+          } else if (path == '24h') {
+            that.setData({
+              weatherHour: res.data.hourly
+            })
+          } else if (path == '7d') {
+            that.setData({
+              weatherDaily: res.data.daily
+            })
+
+
+          }
+
+        }
+      }
+    })
   }
 })
